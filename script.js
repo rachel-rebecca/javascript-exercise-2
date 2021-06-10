4f53de7adc058288cda955dc8616775f7567c264
rachel-rebecca <Rarebecc@umich.edu>
2021-06-10 12:30:17 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let name1 = prompt("Enter first name:");
+let name2 = prompt("Enter second name:");
+let name3 = prompt("Enter third name:");
+
+if (name1.length > name2.length && name1.length > name3.length) {
+  console.log(name1 + " has the longest name.");
+} else if (name2.length > name1.length && name2.length > name3.length) {
+  console.log(name2 + " has the longest name.");
+} else if (name3.length > name1.length && name3.length > name2.length) {
+  console.log(name3 + " has the longest name.");
+} else if (name1.length === name2.length && name1.length !== name3.length) {
+  console.log(name1 + " and " + name2 + "are tied for the longest name.");
+} else if (name1.length === name3.length && name1.length !== name2.length) {
+  console.log(name1 + " and " + name3 + "are tied for the longest name.");
+} else if (name2.length === name3.length && name2.length !== name1.length) {
+  console.log(name2 + " and " + name3 + " are tied for the longest name.");
+} else if (name1.length === name2.length && name2.length === name3.length) {
+  console.log(
+    "All three names, " +
+      name1 +
+      ", " +
+      name2 +
+      ", " +
+      "and " +
+      name3 +
+      ", are the same length."
+  );
+}
