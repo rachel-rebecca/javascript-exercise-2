9d9bd7a0c1f0e33bf55b459d9ac5a7737f52edde
rachel-rebecca <Rarebecc@umich.edu>
2021-06-10 16:17:02 -0400

    added spaces

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ if (name1.length > name2.length && name1.length > name3.length) {
 } else if (name3.length > name1.length && name3.length > name2.length) {
   console.log(name3 + " has the longest name.");
 } else if (name1.length === name2.length && name1.length !== name3.length) {
-  console.log(name1 + " and " + name2 + "are tied for the longest name.");
+  console.log(name1 + " and " + name2 + " are tied for the longest name.");
 } else if (name1.length === name3.length && name1.length !== name2.length) {
-  console.log(name1 + " and " + name3 + "are tied for the longest name.");
+  console.log(name1 + " and " + name3 + " are tied for the longest name.");
 } else if (name2.length === name3.length && name2.length !== name1.length) {
   console.log(name2 + " and " + name3 + " are tied for the longest name.");
 } else if (name1.length === name2.length && name2.length === name3.length) {
